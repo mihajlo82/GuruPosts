de8264e6cea399c4d2c475fa71ed359dba8c83bf
Mihajlo <dimitricmihajlo995@gmail.com>
2024-02-23 13:31:59 +0100

    clean

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,20 +1,15 @@
 import * as S from "./LoaderStyled";
-// import { CSSProperties } from "react";
 import ClipLoader from "react-spinners/ClipLoader";
 
-// const override: CSSProperties = {
-//   display: "block",
-//   margin: "0 auto",
-//   borderColor: "red",
-// };
 
 const Loader = () => {
+  console.log("LOADER POSTER")
+
   return (
     <S.LoaderLayout> 
         <ClipLoader
           color={"green"}
           loading={true}
-        //  cssOverride={override}
           size={200}
           aria-label="Loading Spinner"
           data-testid="loader"
